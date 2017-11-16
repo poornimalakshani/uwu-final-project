2378ed08e9fa5da25fba8dc4ee23b6749945da24
Mohan Wickramasinghe <mohanwickramasinghe@gmail.com>
2017-11-16 17:10:42 +0530

    fixed SQL issue

--- application/models/weight_height_bmi_model.php
+++ application/models/weight_height_bmi_model.php
@@ -13,7 +13,7 @@ class weight_height_bmi_model extends MY_Model {
 
         $id = array('-CHOOSE-');
         $field = array('-CHOOSE-');
-        
+
         for ($i = 0; $i < count($result); $i++)
         {
             array_push($id, $result[$i]->id);
@@ -23,28 +23,29 @@ class weight_height_bmi_model extends MY_Model {
     }
 
      public function filterNormalWeightChildren(){
-        $this->db->select( " p.fullName");
-        $this->db->where("(w.age_duration = 100 and 3.4< w.weight <4.8) or 
-            (w.age_duration = 101 and 4.8< w.weight <5.8) or 
-            (w.age_duration = 102 and 5.2< w.weight <6.6) or 
-            (w.age_duration = 103 and 5.7< w.weight <7.3) or 
-            (w.age_duration = 104 and 6.0< w.weight <7.8) or 
-            (w.age_duration = 105 and 6.5< w.weight <8.2) or 
-            (w.age_duration = 106 and 6.8< w.weight <8.6) or 
-            (w.age_duration = 107 and 7.0< w.weight <9.0) or 
-            (w.age_duration = 108 and 7.3< w.weight <9.3) or
-             (w.age_duration = 109 and 7.5< w.weight <9.5) or 
-            (w.age_duration = 110 and 7.8< w.weight <9.8) or
-             (w.age_duration = 111 and 8.0< w.weight <10.0) or 
-            (w.age_duration = 114 and 8.5< w.weight <10.8) or 
-            (w.age_duration = 117 and 9.0< w.weight <11.5) or 
-            (w.age_duration = 120 and 9.6< w.weight <12.4) or 
-            (w.age_duration = 123 and 10.2< w.weight <13.0)");
+        $this->db->select( "p.fullName");
+        $this->db->where("(w.age_duration = 100 AND (w.weight BETWEEN 3.4 AND 4.8))");
+				$this->db->or_where("(w.age_duration = 101 AND (w.weight BETWEEN 4.8 AND 5.8))");
+				$this->db->or_where("(w.age_duration = 102 AND (w.weight BETWEEN 5.2 AND 6.6))");
+				$this->db->or_where("(w.age_duration = 103 AND (w.weight BETWEEN 5.7 AND 7.3))");
+				$this->db->or_where("(w.age_duration = 104 AND (w.weight BETWEEN 6 AND 7.8))");
+				$this->db->or_where("(w.age_duration = 105 AND (w.weight BETWEEN 6.5 AND 8.2))");
+				$this->db->or_where("(w.age_duration = 106 AND (w.weight BETWEEN 6.8 AND 8.6))");
+				$this->db->or_where("(w.age_duration = 107 AND (w.weight BETWEEN 7 AND 9))");
+				$this->db->or_where("(w.age_duration = 108 AND (w.weight BETWEEN 7.3 AND 9.3))");
+				$this->db->or_where("(w.age_duration = 109 AND (w.weight BETWEEN 7.5 AND 9.5))");
+				$this->db->or_where("(w.age_duration = 110 AND (w.weight BETWEEN 7.8 AND 9.8))");
+				$this->db->or_where("(w.age_duration = 111 AND (w.weight BETWEEN 8 AND 10))");
+				$this->db->or_where("(w.age_duration = 114 AND (w.weight BETWEEN 8.5 AND 10.8))");
+				$this->db->or_where("(w.age_duration = 117 AND (w.weight BETWEEN 9 AND 11.5))");
+				$this->db->or_where("(w.age_duration = 120 AND (w.weight BETWEEN 9.6 AND 12.4))");
+				$this->db->or_where("(w.age_duration = 123 AND (w.weight BETWEEN 10.2 AND 13))");
 
         $this->db->join('reg_child r', 'w.reg_child_id = r.id', 'INNER');
         $this->db->join('people p', 'r.people_id = p.id', 'INNER');
-        
 
+				$this->db->group_by('p.fullName');
+				
         $query = $this->db->get('weight_height_bmi w');
 
         $result = $query->result();
