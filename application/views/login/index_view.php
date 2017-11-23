f4321844e61c6189aa63d20f1c61491614840640
Mohan Wickramasinghe <mohanwickramasinghe@gmail.com>
2017-11-23 11:42:42 +0530

    login css updated

--- application/views/login/index_view.php
+++ application/views/login/index_view.php
@@ -1,27 +1,30 @@
-<?php 
-$this->load->view('layouts/header');
-echo '<div class="login">';
-	echo validation_errors();
-    echo form_open(''); //create form for login
+<?php $this->load->view('layouts/header'); ?>
+<div class="login">
+	<?=form_open('') ?>
+		<div class="bg-danger text-white error-text">
+			<?=validation_errors(); ?>
+		</div>
+		<div class="form-group">
+		  <label for="email">User Name:</label>
+		  <?=form_input(array(
+				'type' => 'text',
+				'name' => 'username',
+				'class'=> 'form-control',
+				'value' => set_value('username')
+			)); ?>
+		</div>
 
-    echo form_label('User Name :');
-    echo form_input(array(
-    	'type' => 'text',
-    	'name' => 'username',
-    	'value' => set_value('username')
-    ));
-    echo "</br>";
+		<div class="form-group">
+		  <label for="email">Passward:</label>
+		  <?=form_input(array(
+				'type' => 'password',
+				'name' => 'password',
+				'class'=> 'form-control',
+			)); ?>
+		</div>
 
-    echo form_label('Passward :');
-    echo form_input(array(
-    	'type' => 'password',
-    	'name' => 'password'
-    ));
-    echo "</br>";
+		<button type="submit" class="btn btn-default btn-sm">Login</button>
+	<?=form_close('') ?>
+</div>
 
-    echo form_submit('submit', 'Login');
-    echo form_close('');
-echo '</div>';
-$this->load->view('layouts/footer'); ?>    
-
-    
+<?php $this->load->view('layouts/footer'); ?>    
