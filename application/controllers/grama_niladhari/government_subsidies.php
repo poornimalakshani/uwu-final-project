5ac074055fd1cb1d1da7c9b8e08b3d3187fc10b3
Mohan Wickramasinghe <mohanwickramasinghe@gmail.com>
2017-11-23 22:12:18 +0530

    ui issues fixed

--- application/controllers/grama_niladhari/government_subsidies.php
+++ application/controllers/grama_niladhari/government_subsidies.php
@@ -10,44 +10,95 @@ class Government_subsidies extends MY_Controller {
         $this->load->library('form_validation');
         $this->load->database();
         $this->load->model('government_subsidies_model');
+		$this->load->model('home_model');
+		$this->load->model('list_model');
     }
 
-
-	public function index()
+	public function index($homeId = 0)
 	{
+		$data = array();
+		$data['homeId'] = $homeId;
+		$data['home'] = $this->home_model->getAll('home', 'address ASC');
+		$data['governmentSubsidies'] = $this->government_subsidies_model->getByWhere('government_subsidies', 'home_id', $homeId);
 
-		$data['field_list'] = $this->government_subsidies_model->getCategory();
-		//$data = array();
-		//$this->load->model('people1_model');
-		
-		$this->form_validation->set_rules('type','Subsidies Type','callback_validate_dropdown');
-		$this->form_validation->set_rules('home_id','Home_Id','trim|required');
-
-		if ($this->form_validation->run() != FALSE) {
-			$insert = [
-				'type' => $this->input->post('type'), 
-				'home_id' => $this->input->post('home_id')
-			];
-			
-			$government_subsidies = $this->government_subsidies_model->insert('government_subsidies', $insert);
-
-			redirect('/grama_niladhari/government_subsidies');
-		}
-
-		$data['government_subsidies'] = $this->government_subsidies_model->getAll('government_subsidies');
+		$data['governmentSubsidiesTypes'] = $this->list_model->getListFields('Government Subsidies');
 
 		$data['menu'] = "grama-niladhari";
 		$data['submenu'] = "grama-niladhari-subsidies-type";
+
 		$this->load->view('grama_niladhari/government_subsidies/government_subsidies_view', $data);
 	}
 
-	function validate_dropdown($str)
-    {
-        if ($str == '-CHOOSE-') {
-            //$this->form_validation->set_message('validate_dropdown', 'Please choose a valid %s');
-            return FALSE;
-        } else {
-            return TRUE;
-        }
-    }
+	public function add_edit($homeId, $subsidiesId = 0) {
+		$data = [];
+
+		if (empty($homeId)) {
+			$this->session->set_flashdata('popup_message', [
+				'type' => 'danger',
+				'message' => 'Sorry, No Home Found!'
+			]);
+
+			echo 'reload';
+			die();
+		}
+
+		$this->form_validation->set_rules('type','Subsidies Type','required');
+
+		$data['home'] = $this->home_model->getByWhere('home', 'id', $homeId, TRUE);
+		$data['governmentSubsidies'] = $this->government_subsidies_model->getByWhere('government_subsidies', 'id', $subsidiesId, TRUE);
+
+		if ($this->form_validation->run() != FALSE) {
+			$insert = [
+				'type' => $this->input->post('type'),
+				'home_id' => $homeId
+			];
+
+			if ($subsidiesId == 0) {
+				$this->government_subsidies_model->insert('government_subsidies', $insert);
+
+				$this->session->set_flashdata('popup_message', [
+					'type' => 'success',
+					'message' => 'New Government Subsidies Type Added!'
+				]);
+			} else {
+				$this->government_subsidies_model->update('government_subsidies', 'id', $subsidiesId, $insert);
+
+				$this->session->set_flashdata('popup_message', [
+					'type' => 'success',
+					'message' => 'Government Subsidies Type Updated!'
+				]);
+			}
+
+			echo 'success';
+			die();
+		}
+
+		$data['governmentSubsidiesTypes'] = $this->list_model->getListFields('Government Subsidies');
+
+		$this->load->view('grama_niladhari/government_subsidies/add_edit_view', $data);
+	}
+
+	public function delete($subsidiesId = 0)
+	{
+		$data['governmentSubsidies'] = $this->government_subsidies_model->getByWhere('government_subsidies', 'id', $subsidiesId, TRUE);
+
+		$homeId = '';
+		if ($data['governmentSubsidies']) {
+			$this->government_subsidies_model->delete('government_subsidies', 'id', $subsidiesId);
+
+			$this->session->set_flashdata('popup_message', [
+				'type' => 'success',
+				'message' => 'Government Subsidies Type Deleted!'
+			]);
+
+			$homeId = $data['governmentSubsidies']->home_id;
+		} else {
+			$this->session->set_flashdata('popup_message', [
+				'type' => 'danger',
+				'message' => 'Sorry, No Government Subsidies Type Found!'
+			]);
+		}
+
+		redirect('/grama_niladhari/government_subsidies/'.$homeId);
+	}
 } 
--- application/views/grama_niladhari/government_subsidies/government_subsidies_view.php
+++ application/views/grama_niladhari/government_subsidies/government_subsidies_view.php
@@ -1,46 +1,58 @@
-<?php 
-$this->load->view('layouts/header');
-?>  
+<?php $this->load->view('layouts/header'); ?>
 
-<div class="container">
-        <?php
-        echo validation_errors();
-        echo form_open('');
+<div class="row">
+	<div class="col-md-12">
+	<h4>
+		Government Subsidies
+		<?php if(!empty($homeId)) { ?>
+		<span class="pull-right"><small><a href="/grama_niladhari/government_subsidies/add_edit/<?=$homeId?>" data-type="add" class="btn btn-success add-edit">Add New</a></small></span>
+		<?php } ?>
+	</h4>
+	</div>
+	<div class="col-md-12">
+		<div class="form-group">
+			<select class="form-control" id="home-select">
+				<option value="0">-- Select Home --</option>
 
-             echo form_label('Subsidies Type');
+				<?php foreach($home as $x) { ?>
+				<option value="<?=$x->id?>" <?=($x->id == $homeId) ? 'selected="selected"': ''; ?>><?=$x->address?></option>
+				<?php } ?>
+			</select>
+		</div>
 
-             $data=array(
-             		'41' => 'Samurdhi',
-             		'42'   => 'Furtilizer Subsidies',
-                    '43'   => 'Social Services Subsidies',
-                    '44' => 'Gardening Aids',
-                    '45' => 'Cancer Aids',
-                    '324' => 'Mahapola',
-                    '325' => 'Bursary',
-                    );
-             echo form_dropdown('type', $data, 'Samurdhi');
-             echo "</br>";
-             
-
-             echo form_label('Home_Id:');
-
-             $data=array(
-             		'name' => 'home_id',
-             		'id'   => 'home_id',
-             		'value' => ''
-             	);
-             
-             echo form_input($data);
-             echo "</br>";
-             
-             echo form_submit('submit', 'Save');
-            echo form_close('');
-        ?>
-
+		<?php if(empty($governmentSubsidies)) { ?>
+			<p class="text-warning"><?=(empty($homeId) ? 'Please Select A Home!' : 'Sorry, No Government Subsidies Types!')?></p>
+		<?php } else { ?>
+		<table class="table table-striped">
+			<tbody>
+		    <?php foreach($governmentSubsidies as $x) { ?>
+				<tr>
+					<td><?=$governmentSubsidiesTypes[$x->type]?></td>
+					<td align="right">
+						<!--<a class="action" href="/grama_niladhari/people1/<?=$x->id?>"><i class="fa fa-eye fa-lg"></i></a>-->
+						<a class="action add-edit" data-type="edit" href="/grama_niladhari/government_subsidies/add_edit/<?=$homeId?>/<?=$x->id?>"><i class="fa fa-pencil fa-lg"></i></a>
+						<a class="action delete" href="/grama_niladhari/government_subsidies/delete/<?=$x->id?>"><i class="fa fa-trash fa-lg"></i></a>
+					</td>
+				</tr>
+			<?php } ?>
+			</tbody>
+		</table>
+		<?php } ?>
     </div>
 </div>
 
+<?php $this->load->view('layouts/dialog'); ?>
+
 <?php $this->load->view('layouts/footer'); ?>   
 
+<script type="text/javascript">
+pageName = "Government Subsidies Type";
 
+$(document).ready(function() {
+	$('#home-select').on('change', function(e) {
+		e.preventDefault();
 
+		window.location.href = "/grama_niladhari/government_subsidies/"+$(this).val();
+	});
+});
+</script>
