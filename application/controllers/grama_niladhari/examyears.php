f07f5ada9e5d3dfd3dedf633754b66f483d95f25
Mohan Wickramasinghe <mohanwickramasinghe@gmail.com>
2017-11-24 09:24:45 +0530

    ui issues fixed

--- application/controllers/grama_niladhari/examyears.php
+++ application/controllers/grama_niladhari/examyears.php
@@ -124,47 +124,4 @@ class ExamYears extends MY_Controller {
 		}
 
 	}
-/*
-	public function index()
-	{
-		$data['field_list'] = $this->examyears_model->getCategory();
-		//$data = array();
-		//$this->load->model('people1_model');
-
-		$this->form_validation->set_rules('examType','Exam Type','callback_validate_dropdown');
-		$this->form_validation->set_rules('year','Year','trim|required');
-		$this->form_validation->set_rules('people_id','People Id','trim|required');
-		$this->form_validation->set_rules('people_home_id','Home_Id','trim|required');
-
-		if ($this->form_validation->run() != FALSE) {
-			$insert = [
-				'examType' => $this->input->post('examType'), 
-				'year' => $this->input->post( 'year'), 
-				'people_id' => $this->input->post('people_id'), 
-				'people_home_id' => $this->input->post('people_home_id'),
-			];
-
-			$examyears = $this->examyears_model->insert('examyears', $insert);
-
-			redirect('/grama_niladhari/examyears');
-		}
-
-		$data['examyears'] = $this->examyears_model->getAll('examyears');
-
-		$data['menu'] = "grama-niladhari";
-		$data['submenu'] = "grama-niladhari-exam-years";
-		$this->load->view('grama_niladhari/examyears/examyears_view', $data);
-
-	}
-
-	function validate_dropdown($str)
-    {
-        if ($str == '-CHOOSE-') {
-            //$this->form_validation->set_message('validate_dropdown', 'Please choose a valid %s');
-            return FALSE;
-        } else {
-            return TRUE;
-        }
-    }
- */
 } 
