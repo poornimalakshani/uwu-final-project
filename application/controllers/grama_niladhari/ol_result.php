f7414307eb70f3e2f9a544812a4141a1ca7c5417
Mohan Wickramasinghe <mohanwickramasinghe@gmail.com>
2017-11-24 08:34:00 +0530

    ui issues fixed

--- application/controllers/grama_niladhari/ol_result.php
+++ application/controllers/grama_niladhari/ol_result.php
@@ -124,45 +124,4 @@ class Ol_result extends MY_Controller {
 		}
 
 	}
-
-//	public function index()
-//	{
-//
-//		$data['field_list'] = $this->ol_result_model->getCategory();
-//		//$data = array();
-//		//$this->load->model('people1_model');
-//
-//		$this->form_validation->set_rules('subject','Subject','callback_validate_dropdown');
-//		$this->form_validation->set_rules('result','Result','trim|required');
-//		$this->form_validation->set_rules('people_id','People Id','trim|required');
-//		$this->form_validation->set_rules('people_home_id','Home_Id','trim|required');
-//
-//		if ($this->form_validation->run() != FALSE) {
-//			$insert = [
-//				'subject' => $this->input->post('subject'),
-//				'result' => $this->input->post('result'),
-//				'people_id' => $this->input->post('people_id'),
-//				'people_home_id' => $this->input->post('people_home_id')
-//				];
-//
-//			$ol_result = $this->ol_result_model->insert('ol_result', $insert);
-//			redirect('/grama_niladhari/ol_result');
-//		}
-//
-//		$data['ol_result'] = $this->ol_result_model->getAll('ol_result');
-//
-//		$data['menu'] = "grama-niladhari";
-//		$data['submenu'] = "grama-niladhari-ol-results";
-//		$this->load->view('grama_niladhari/ol_result/ol_result_view', $data);
-//	}
-//
-//	function validate_dropdown($str)
-//    {
-//        if ($str == '-CHOOSE-') {
-//            //$this->form_validation->set_message('validate_dropdown', 'Please choose a valid %s');
-//            return FALSE;
-//        } else {
-//            return TRUE;
-//        }
-//    }
 } 
