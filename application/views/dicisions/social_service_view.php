ea9f5daf8f7456d02a29ef6169fc712a61b2443c
Mohan Wickramasinghe <mohanwickramasinghe@gmail.com>
2017-11-26 01:06:35 +0530

    dicision UI updated

--- application/views/dicisions/social_service_view.php
+++ application/views/dicisions/social_service_view.php
@@ -2,32 +2,33 @@
 $this->load->view('layouts/header');
 ?>  
 
-  <h2>Social Service Granters</h2>
-  <p>Peoples who grant the social Services:</p>            
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>Full Name</th>
-       
-        <th>Date Of Birth</th>
-        <th>Age</th>
-      </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach ($socialServiceGranters as $key => $value) {
-    	?>
-    
-      <tr>
-
-        <td><?=$value->fullName; ?></td>
-        
-        <td><?=$value->dateOfBirth;?></td>
-        <td><?=(int)$value->ageInYears;?></td>
-      </tr>
-     <?php }?>
-      
-    </tbody>
-  </table>
+<div class="row">
+	<div class="col-md-12">
+		<h4>
+			Social Service Granters<br>
+			<small>Peoples who grant the social Services::</small>
+		</h4>
+	</div>
+	<div class="col-md-12">
+		<table class="table table-striped no-top-border">
+			<thead>
+				<tr>
+					<th>Full Name</th>
+					<th>Date Of Birth</th>
+					<th>Age</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php foreach ($socialServiceGranters as $key => $value) { ?>
+				<tr>
+					<td><?=$value->fullName; ?></td>
+					<td><?=$value->dateOfBirth;?></td>
+					<td><?=(int)$value->ageInYears;?></td>
+				</tr>
+			<?php }?>
+			</tbody>
+		</table>
+	</div>
+</div>
 
 <?php $this->load->view('layouts/footer'); ?>
