ddaebfaa4a80bf8d85ccbb39edd23ad80883d49b
Mohan Wickramasinghe <mohanwickramasinghe@gmail.com>
2017-11-26 01:02:07 +0530

    dicision UI updated

--- application/views/dicisions/samurdhi_view.php
+++ application/views/dicisions/samurdhi_view.php
@@ -1,31 +1,37 @@
 <?php 
 $this->load->view('layouts/header');
-?>  
-  <h2>Samurdhi Granters</h2>
-  <p>Peoples who grant the Samurdhi:</p>            
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>Home Id</th>
-       
-        <th>Address</th>
-        <th>Income(Rs)</th>
-      </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach ($samurdhiGranters as $key => $value) {
-    	?>
-    
-      <tr>
+?>
 
-        <td><?=$value->id; ?></td>
-        <td><?=$value->address;?></td>
-        <td><?=$value->income;?></td>
-      </tr>
-     <?php }?>
-      
-    </tbody>
-  </table>
-  
- <?php $this->load->view('layouts/footer'); ?>
+<div class="row">
+	<div class="col-md-12">
+		<h4>
+			Samurdhi Granters<br>
+			<small>Peoples who grant the Samurdhi:</small>
+		</h4>
+	</div>
+	<div class="col-md-12">
+		<table class="table table-striped no-top-border">
+			<thead>
+				<tr>
+					<th>Home Id</th>
+					<th>Address</th>
+					<th>Income(Rs)</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php
+			foreach ($samurdhiGranters as $key => $value) {
+			?>
+				<tr>
+					<td><?=$value->id; ?></td>
+					<td><?=$value->address;?></td>
+					<td><?=$value->income;?></td>
+				</tr>
+			<?php }?>
+
+			</tbody>
+		</table>
+	</div>
+</div>
+
+<?php $this->load->view('layouts/footer'); ?>
